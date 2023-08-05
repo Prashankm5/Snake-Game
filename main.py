2a39db6e49595960cdc02989df668202634d0f04
Prashank Mishra <114643833+Prashankm5@users.noreply.github.com>
2023-08-06 00:37:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from turtle import Screen
+from snake import Snake
+import time
+from food import Food
+from scoreboard import ScoreBoard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = ScoreBoard()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        score.game_over()
+
+    ## Detect collision with tail
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+ALIGNMENT='center'
+FONT=('Courier', 24, 'normal')
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score=0
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.write(f'Score : {self.score}', align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over : ", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score+=1
+        self.clear()
+        self.update_score()
